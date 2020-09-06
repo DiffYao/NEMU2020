5c813ce1279a97031bb71a3e815bf0206287a537
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-06 06:27:44 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:27:44 up  3:54,  1 user,  load average: 0.24, 0.87, 0.70 e5dff79dd99d50748f1b0ec884fca1e2831fc59c

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,11 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args);
+
+static int cmd_x(char *args);
+
+static int cmd_info(char *args);
 static struct {
 	char *name;
 	char *description;
@@ -46,13 +51,27 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-
+	{ "si", "Execute N instructions in a single step and then pause, the default is 1", cmd_si},
+	{ "info ", "r for  Disply info about register \n w for print watchpoint infomation", cmd_info},
+	{ "x", "Scanf memory, Disply N Byte", cmd_x}
 	/* TODO: Add more commands */
 
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
-
+static int cmd_info(char *args){
+	return 0;
+}
+static int cmd_x(char *args){
+	return 0;
+}
+static int cmd_si(char *args){
+	char *arg = strtok(NULL, " ");
+	int i;
+  	sscanf(arg, "%d", &i);
+	cpu_exec(i);
+	return 0;
+}
 static int cmd_help(char *args) {
 	/* extract the first argument */
 	char *arg = strtok(NULL, " ");
