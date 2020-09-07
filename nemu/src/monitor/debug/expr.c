af7888a034a87e87593f2fd82ca3e7bcb6758d81
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-07 12:26:08 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:26:08 up 1 day,  9:52,  1 user,  load average: 0.45, 0.33, 0.36 2e681cb110db903a6ec75d4be5ccd0902d31ea12

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -119,7 +119,10 @@ bool check_parentheses(int p, int q)
 		for (i = p + 1; i < q; i++){
 			if (tokens[i].type == '(') count++;
 			if (tokens[i].type == ')') count--;
-			if (count < 0) return false;
+			if (count < 0) {
+				panic("Parentheses No Matching\n");
+				return false;
+			}
 		}
 		if (count == 0) return true;
 	}
