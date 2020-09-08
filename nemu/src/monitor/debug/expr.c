f043dc9ed798b83c0d178340d576e9ef3f7bc107
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-08 06:40:13 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:40:13 up 2 days,  4:06,  1 user,  load average: 0.41, 0.32, 0.24 c49a89f2e7b981be1d1f0b194103c4ad0546940

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -25,7 +25,7 @@ static struct rule {
         {"\\(", '('},                                   // leftpa 40
         {"\\)", ')'},                                   // rightpa 41
 	{"[0-9]+", NUM},				   // number 258 
-	{"\\b0[xX][0-9a-fA-F]+\\b", HEXNUM},			   // hexadecimal number 259
+	{"0x[0-9a-fA-F]+", HEXNUM},			   // hexadecimal number 259
 
 	{" +",	NOTYPE},				// spaces 256
 	{"\\+", '+'},					// plus 43 
