e06c36dbb76bbbd2d02e8f70fddb0683816d1e15
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-25 08:51:13 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:51:13 up  5:40,  1 user,  load average: 0.52, 0.45, 0.28 85a30a8eefc8c313fdf098b929d7f61236a2cc69

--- nemu/src/cpu/exec/con-tran/jbe-template.h
+++ nemu/src/cpu/exec/con-tran/jbe-template.h
@@ -6,7 +6,7 @@ static void do_execute(){
 	
 	DATA_TYPE_S displacement = op_src->val;
 	
-	if (cpu.ZF == 1|| cpu.CF == 1) cpu.eip += displacement;
+	if (cpu.CF == 1|| cpu.ZF == 1) cpu.eip += displacement;
 	print_asm("jbe %x", cpu.eip + displacement + 1);
 
 }
