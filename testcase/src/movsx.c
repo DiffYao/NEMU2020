acb07fe2573ce649d97748f2bbd6c8a6ccef733f
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 06:55:32 +0000

    >  run 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:55:32 up 1 day,  3:44,  1 user,  load average: 0.48, 0.30, 0.26 6deed38a0c30f5bba20b12ff445591914d8c30d

--- testcase/src/movsx.c
+++ testcase/src/movsx.c
@@ -1,6 +1,7 @@
 #include "trap.h"
 
 int A[10];
+int b;
 char C[10];
 int main() {
 	A[0] = 0;
@@ -13,14 +14,13 @@ int main() {
 	nemu_assert(C[1] == 'a');
 
 	A[0] = (int)C[0];
-	nemu_assert(A[0] ==  0x00000061);
-
+	nemu_assert(A[0] ==  0x61);
 
 	C[1] = 0x80;
 	A[0] = (int)C[1];
+
 	nemu_assert(C[1] == 0xffffff80);
 	nemu_assert(A[0] == 0xffffff80);
-
 
 
 	return 0;
