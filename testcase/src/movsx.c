b92831705ab15812e3e08d1a9d5f7c36628e3667
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 06:34:58 +0000

    >  run 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:34:58 up 1 day,  3:24,  1 user,  load average: 0.21, 0.26, 0.46 ab2e16c58050b79d41e0dd67af92a60e398134a3

--- testcase/src/movsx.c
+++ testcase/src/movsx.c
@@ -23,6 +23,10 @@ int main() {
 
 	C[1] = 0x80;
 	A[0] = (int)C[1];
+
+	nemu_assert(C[1] == 0xffffff80);
+	nemu_assert(A[0] == 0xffffff80);
+
 /*
 	nemu_assert(A[1] == 1);
 	nemu_assert(A[2] == 2);
@@ -32,9 +36,7 @@ int main() {
 	nemu_assert(b == 3);
 	nemu_assert(A[5] == 3);
 */
-	nemu_assert(C[1] == 0xffffff80);
 
-	nemu_assert(A[0] == 0xffffff80);
 
 	return 0;
 }
