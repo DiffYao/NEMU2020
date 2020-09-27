06e9b800ae70b0e18193efcaa89463557ddad65c
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 07:10:24 +0000

    >  run 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:10:24 up 2 days,  3:59,  1 user,  load average: 0.64, 0.57, 0.49 265a60a46955e77fd4bcd414cdac5e132eb53a28

--- testcase/src/if-else.c
+++ testcase/src/if-else.c
@@ -18,12 +18,12 @@ int ans[] = {0, 0, 0, 0, 50, 50, 50, 75, 75, 75, 100, 100, 100, 150};
 //(sizeof(test_data) / sizeof(test_data[0]))
 
 int main() {
-	int i = 0, ans_idx = 0;
+//	int i = 0;
 //	for(i = 0; i < NR_DATA; i ++) {
 //		nemu_assert(if_else(test_data[i]) == ans[ans_idx ++]);
 //	}
-	nemu_assert(if_else(test_data[0]) == ans[ans_idx ++]);
-	nemu_assert(i == NR_DATA);
+	nemu_assert(if_else(test_data[200]) == ans[8]);
+//	nemu_assert(i == NR_DATA);
 
 	return 0;
 }
