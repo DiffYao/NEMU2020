9d44600f16c9e3cd9703d4c73fe94e650dcf71f4
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 07:37:22 +0000

    >  run 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:37:22 up 2 days,  4:26,  1 user,  load average: 0.42, 0.43, 0.47 15e45748e951c67054f0b994e001641262a59f57

--- testcase/src/add-longlong.c
+++ testcase/src/add-longlong.c
@@ -13,7 +13,8 @@ long long ans[] = {0LL, 0x1LL, 0x2LL, 0x7fffffffffffffffLL, 0x8000000000000000LL
 
 int main() {
 	
-	nemu_assert(add(test_data[0], test_data[1]) == ans[1]);
+	nemu_assert(test_data[1] == ans[1]);
+ 	//nemu_assert(add(test_data[0], test_data[1]) == ans[1]);
 /*
 	int i, j, ans_idx = 0;
 	int loop = 0;
