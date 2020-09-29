f7b93a41f8f3b66f915127da5ab3e467c6777437
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-29 14:24:51 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:24:52 up 4 days, 11:14,  1 user,  load average: 0.97, 0.54, 0.29 cd92c5fe2c7dbc75593ad1317ba364c73e209516

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -7,37 +7,35 @@ static void do_execute() {
 	DATA_TYPE_S IncDec;
 
 	
-	if (DATA_BYTE == 1){
-	
-		reg_b(R_AL) = MEM_R (reg_w(R_SI));
+	DATA_TYPE src =  reg_l(R_ESI);
+	if (DATA_BYTE == 1) {
 
-		if (cpu.DF == 0) IncDec = 1;
-		else 		 IncDec = -1;
+		reg_b(R_AL) = MEM_R(src);
 
-		reg_w (R_SI) += IncDec;
+		if (cpu.DF == 0) 	IncDec = 1;
+		else 		 	IncDec = -1;
 
 	}
 	else if (DATA_BYTE == 2)
 	{
 		
-		reg_w(R_AX) = MEM_R (reg_w(R_SI));
+		reg_w(R_AX) = MEM_R (src);
 
-		if (cpu.DF == 0) IncDec = 2;
-		else 		 IncDec = -2;
+		if (cpu.DF == 0)	IncDec = 2;
+		else 		 	IncDec = -2;
 
-		reg_w (R_SI) += IncDec;
 	}
 	else
 	{
 		
-		reg_l(R_EAX) = MEM_R (reg_l(R_ESI));
+		reg_l(R_EAX) = MEM_R (src);
 
 		if (cpu.DF == 0)	IncDec = 4;
 		else 			IncDec = -4;
 
-		reg_l (R_ESI) += IncDec;
 
 	}
+	src += IncDec;
 	print_asm("lods");
 
 }
