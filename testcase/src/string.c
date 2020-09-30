7ddaad1b7ea26aef818b27e9ccc4ccdcdbea4f8e
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 09:15:24 +0000

    >  run 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:15:24 up 5 days,  6:04,  1 user,  load average: 1.06, 0.71, 0.63 83e9c4e0de5259d384c1294e2611960c168cb605

--- testcase/src/string.c
+++ testcase/src/string.c
@@ -15,7 +15,7 @@ char str[20];
 
 
 int main() {
-/*
+
 	nemu_assert(strcmp(s[0], s[2]) == 0);
 
 	nemu_assert(strcmp(s[0], s[1]) == -1);
@@ -23,13 +23,12 @@ int main() {
 	nemu_assert(strcmp(s[0] + 1, s[1] + 1) == -1);
 	nemu_assert(strcmp(s[0] + 2, s[1] + 2) == -1);
 	nemu_assert(strcmp(s[0] + 3, s[1] + 3) == -1);
-*/
-	//nemu_assert(strcmp(strcpy(str, str1), "Hello") == 0);
-	//char str9[10] = "a";
 
-	//nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
+	nemu_assert(strcmp(strcpy(str, str1), "Hello") == 0);
 
-	nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
+	nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
+
+	//nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
 	//memset(str, '#', 5);
 	//nemu_assert(strcmp(str, s[5]) == 0);
 
