b2c9069ecb7085a2e9776ad376d5f6c198eae996
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 01:06:43 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:06:43 up 4 days, 21:56,  1 user,  load average: 0.52, 0.64, 0.48 dbb7f92c4f7050e146b61ddec415d9133c0d52f

--- nemu/src/cpu/exec/con-tran/jne-template.h
+++ nemu/src/cpu/exec/con-tran/jne-template.h
@@ -5,9 +5,9 @@
 static void do_execute(){
 	
 	DATA_TYPE_S displacement = op_src->val;
-	
-	if (cpu.ZF == 0) cpu.eip += displacement;
 	print_asm("jne %x", cpu.eip + displacement + DATA_BYTE + 1);
+	if (cpu.ZF == 0) cpu.eip += displacement;
+	
 
 }
 
