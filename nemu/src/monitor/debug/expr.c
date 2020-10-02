1cfe78444bba87a43dc45ef369d5e48c986a72a5
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-02 13:14:57 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:14:57 up 7 days, 10:04,  1 user,  load average: 0.57, 0.48, 0.48 5ddae29161f9dfd0f2904ad38276c46fc4e70645

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -188,6 +188,7 @@ uint32_t eval (int p, int q)
 					Log("2\n");
 					char* temp2 = strtab + symtab[i].st_name;
 					printf("str = %s, len = %x\n", temp2, (int)strlen(temp2));
+					printf("tmp2len = %d\n", tmplen);
 					strncpy (tmp, temp2 ,tmplen);
 					Log("3\n");
 					tmp [tmplen] = '\0';
