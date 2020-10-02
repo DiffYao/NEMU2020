b5242d1d69dcc46f5024045338d43801bad8b30d
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-02 12:40:55 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:40:55 up 7 days,  9:30,  1 user,  load average: 0.95, 0.91, 0.80 f4575ed0e0075f49c54f261d2c9b515bc31f06df

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -181,7 +181,7 @@ uint32_t eval (int p, int q)
 			{
 				if ((symtab[i].st_info & 0xf) == STT_OBJECT)
 				{
-					char tmp [30];
+					char tmp [300];
 					int tmplen = symtab[i+1].st_name - symtab[i].st_name - 1;
 					strncpy (tmp, strtab + symtab[i].st_name,tmplen);
 					tmp [tmplen] = '\0';
