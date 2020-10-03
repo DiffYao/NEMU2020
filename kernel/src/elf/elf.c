d7394770b8de558a8d5bf59fca12fbbdde876d4c
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-03 08:22:55 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:22:55 up  4:58,  1 user,  load average: 0.76, 0.67, 0.36 1bf01cb4fde3a0fb33c3606a37cbf5d0a6f408e5

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -65,7 +65,7 @@ uint32_t loader() {
 			/* Record the program break for future use. */
 			extern uint32_t cur_brk, max_brk;
 			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
-			if(cur_brk < new_brk) { max_brk = cur_brk = new_brk; }
+			if(cur_brk < new_brk) { cur_brk = new_brk; max_brk = new_brk; }
 #endif
 		}
 	}
