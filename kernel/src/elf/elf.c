8c82812df4d04af183d4644c4893e816a3ece884
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-03 04:53:52 +0000

    >  compile kernel 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:53:52 up  1:29,  1 user,  load average: 0.45, 0.40, 0.31 a7f408f7f85142e8d2d8807942b9582c41f73843

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -33,25 +33,32 @@ uint32_t loader() {
 
 	/* TODO: fix the magic number with the correct one */
 	const uint32_t elf_magic = 0x464c457f;
-//	Log("elf is 0x%x\n, elf_magic");
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
 	//panic("please implement me");
 	ph = (void *)(buf + elf->e_phoff);
-	for(; true; ) {
+	int i;
+	for(i = 0; i < elf->e_phnum ; ++i,++ph) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 			ph->p_vaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
+
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 
+#ifdef HAS_DEVICE
+			ide_read((void *)ph->p_vaddr,ph->p_offset,ph->p_filesz);
+#else	
+			ramdisk_read ((void *)ph->p_vaddr,ph->p_offset,ph->p_filesz);
+#endif				 
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
+
+			 memset ((void *)(ph->p_vaddr + ph->p_filesz),0,ph->p_memsz -ph->p_filesz);
 
 
 #ifdef IA32_PAGE
