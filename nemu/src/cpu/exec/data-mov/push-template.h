b0dcb91f1b919c46011b9cf0411b195dec986a79
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 11:06:15 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:06:15 up  8:17,  1 user,  load average: 0.26, 0.38, 0.34 10a8993be11d67c82aeb88f272d8993110542692

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -3,12 +3,18 @@
 #define instr push
 
 static void do_execute() {
+	if (DATA_BYTE == 1)
+		op_src->val = (int8_t) op_src->val;	
 	reg_l (R_ESP) -= 4;
 	swaddr_write(reg_l (R_ESP), 4, op_src->val);
 	print_asm_template1();
 }
 
-make_instr_helper(r);
 make_instr_helper(i);
+
+#if DATA_BYTE != 1
+make_instr_helper(r);
 make_instr_helper(rm);
+#endif
+
 #include "cpu/exec/template-end.h"
