356f25947de519996aec6397e3fdc8734459b561
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 09:17:30 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:17:30 up  6:28,  1 user,  load average: 0.43, 0.67, 0.73 3b494aa5cba2dfe852d83e36825d5aaa96adf6b2

--- nemu/src/cpu/exec/con-tran/jmp-template.h
+++ nemu/src/cpu/exec/con-tran/jmp-template.h
@@ -4,12 +4,12 @@
 
 static void do_execute(){
 	
-	DATA_TYPE_S displacement = op_src->val;
+	DATA_TYPE displacement = op_src->val;
 	if (op_src->type == OP_TYPE_MEM || op_src->type == OP_TYPE_REG){
 		cpu.eip = displacement - concat(decode_rm_, SUFFIX)(cpu.eip+1) -1;
 	}
 	else {
-		cpu.eip += displacement;
+		cpu.eip += (DATA_TYPE_S)displacement;
 	}
 	print_asm("jmp %x", cpu.eip + 1 + DATA_BYTE);
 }
