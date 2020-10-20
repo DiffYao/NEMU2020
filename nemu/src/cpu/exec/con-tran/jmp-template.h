fe708098b7c317c5f97afc3877898c421d4354e2
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 12:54:42 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:54:42 up 9 days,  5:37,  1 user,  load average: 1.37, 1.09, 1.01 360b06614fb1d2b41bb911f8aeaed2cca1362d57

--- nemu/src/cpu/exec/con-tran/jmp-template.h
+++ nemu/src/cpu/exec/con-tran/jmp-template.h
@@ -26,16 +26,18 @@ make_helper(concat(jmp_l_, SUFFIX)) {
 	swaddr_t offset = instr_fetch(eip + 1, DATA_BYTE);
 	uint16_t cs_new = instr_fetch(eip + 5, 2);
 	cpu.eip = offset - DATA_BYTE - 3;
+	printf("index = %x\n", cpu.sreg[1].INDEX);
 #if DATA_BYTE == 2
 	cpu.eip = cpu.eip & 0x0000ffff;
 #endif
+/*
 	cpu.sreg[1].selector = cs_new;
 	cpu.sreg[1].cache.base_15_0 = lnaddr_read(cpu.gdtr.base + 8 * cpu.sreg[1].INDEX + 2, 2);
 	cpu.sreg[1].cache.base_23_16 = lnaddr_read(cpu.gdtr.base + 8 * cpu.sreg[1].INDEX + 4, 1);
 	cpu.sreg[1].cache.base_31_24 = lnaddr_read(cpu.gdtr.base + 8 * cpu.sreg[1].INDEX + 7, 1);
 	cpu.sreg[1].cache.limit_15_0 = lnaddr_read(cpu.gdtr.base + 8 * cpu.sreg[1].INDEX, 2);
 	cpu.sreg[1].cache.limit_19_16 = lnaddr_read(cpu.gdtr.base + 8 * cpu.sreg[1].INDEX + 6, 1) & 0xf;
-
+*/
 	print_asm("ljmp $%hx,$%x", cs_new, offset);
 	return DATA_BYTE + 3;
 }
