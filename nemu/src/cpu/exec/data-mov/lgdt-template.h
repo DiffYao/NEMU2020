cc01e7e72ac329747bcdb077e29d621195da1993
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 12:28:49 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:28:49 up 9 days,  5:11,  1 user,  load average: 0.99, 1.05, 1.14 b69b6e8bbd3f9f32e4f8e8cc5b776222d21497ba

--- nemu/src/cpu/exec/data-mov/lgdt-template.h
+++ nemu/src/cpu/exec/data-mov/lgdt-template.h
@@ -9,6 +9,7 @@ make_helper(concat(lgdt_, SUFFIX)) {
 		cpu.gdtr.base = lnaddr_read(op_src->addr + 2, 3);
 	else 
 		cpu.gdtr.base = lnaddr_read(op_src->addr + 2, 4);
+
 	print_asm("lgdt 0x%hx 0x%x", cpu.gdtr.limit, cpu.gdtr.base);
 	return len + 1;
 }
