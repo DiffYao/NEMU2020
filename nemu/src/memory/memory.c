64a049f75ff321b1d6da67a2f445a55786375289
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 11:59:31 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:59:31 up 9 days,  4:41,  1 user,  load average: 1.07, 0.99, 0.91 306cf95c18f5b5d8db2e1a6ef0c0ae975f4d44cf

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,13 +12,13 @@ void dram_write(hwaddr_t, size_t, uint32_t);
 
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-	//return cache1_read(addr, len) & (~0u >> ((4 - len) << 3));
+	//return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	return cache1_read(addr, len) & (~0u >> ((4 - len) << 3));
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	return dram_write(addr, len, data);
-	//return cache1_write(addr, len, data);
+	//return dram_write(addr, len, data);
+	return cache1_write(addr, len, data);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
