a61bc02049f9aecb05d6b0fcae54fcc9c7835477
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-20 16:33:03 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:33:03 up 7 days, 10:06,  1 user,  load average: 0.98, 1.23, 1.32 48bf92a020d2ce61928530efc766c2f465993881

---
+++ nemu/src/cpu/exec/intr.c
@@ -0,0 +1,30 @@
+#include "nemu.h"
+
+#include <setjmp.h>
+extern jmp_buf jbuf;
+void raise_intr(uint8_t NO) {
+
+	cpu.esp -= 4;
+	swaddr_write(cpu.esp, 4, cpu.eflags, 2);
+	cpu.esp -= 4;
+	swaddr_write(cpu.esp, 4, cpu.sreg[1].selector, 2);
+	cpu.esp -= 4;
+	swaddr_write(cpu.esp, 4, cpu.eip, 2);
+
+	uint16_t idt_selector = lnaddr_read(cpu.idtr.base + 8 * NO + 2, 2);
+	cpu.sreg[1].selector = idt_selector;
+	cpu.sreg[1].cache.base_15_0 = lnaddr_read(cpu.gdtr.base + 8 * cpu.sreg[1].INDEX + 2, 2);
+	cpu.sreg[1].cache.base_23_16 = lnaddr_read(cpu.gdtr.base + 8 * cpu.sreg[1].INDEX + 4, 1);
+	cpu.sreg[1].cache.base_31_24 = lnaddr_read(cpu.gdtr.base + 8 * cpu.sreg[1].INDEX + 7, 1);
+	cpu.sreg[1].cache.limit_15_0 = lnaddr_read(cpu.gdtr.base + 8 * cpu.sreg[1].INDEX, 2);
+	cpu.sreg[1].cache.limit_19_16 = lnaddr_read(cpu.gdtr.base + 8 * cpu.sreg[1].INDEX + 6, 1) & 0xf;
+
+	uint32_t base_addr = (cpu.sreg[1].cache.base_31_24 << 24) | (cpu.sreg[1].cache.base_23_16 << 16) | cpu.sreg[1].cache.base_15_0;
+	uint16_t offset1 = lnaddr_read(cpu.idtr.base + 8 * NO, 2);
+	uint16_t offset2 = lnaddr_read(cpu.idtr.base + 8 * NO + 6, 2);
+	uint32_t offset_total = (offset2 << 16) | offset1;
+
+	cpu.eip = base_addr + offset_total;
+
+	longjmp(jbuf, 1);
+}
