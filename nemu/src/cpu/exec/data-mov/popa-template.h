4f5a23976a7bea66e42d887808811a9b9acbf15e
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-21 07:52:21 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:52:21 up 8 days,  1:25,  1 user,  load average: 1.85, 1.54, 1.34 72c9cd32133b06a85c296eb726b60ae010dde41f

--- nemu/src/cpu/exec/data-mov/popa-template.h
+++ nemu/src/cpu/exec/data-mov/popa-template.h
@@ -5,21 +5,21 @@
 make_helper(concat(popa_, SUFFIX)) {
 	
 	REG(7) = MEM_R(cpu.esp, 2);
-	cpu.esp += DATA_BYTE;
-	REG(6) = MEM_R(cpu.esp, 2);
-	cpu.esp += DATA_BYTE;
+    cpu.esp += DATA_BYTE;
+    REG(6) = MEM_R(cpu.esp, 2);
+    cpu.esp += DATA_BYTE;
 	REG(5) = MEM_R(cpu.esp, 2);
-	cpu.esp += DATA_BYTE;
-	cpu.esp += DATA_BYTE;
-	REG(3) = MEM_R(cpu.esp, 2);
-	cpu.esp += DATA_BYTE;
-	REG(2) = MEM_R(cpu.esp, 2);
-	cpu.esp += DATA_BYTE;
-	REG(1) = MEM_R(cpu.esp, 2);
-	cpu.esp += DATA_BYTE;
-	REG(0) = MEM_R(cpu.esp, 2);
-	cpu.esp += DATA_BYTE;
-	print_asm("popa");
+    cpu.esp += DATA_BYTE;
+    cpu.esp += DATA_BYTE;
+    REG(3) = MEM_R(cpu.esp, 2);
+    cpu.esp += DATA_BYTE;
+    REG(2) = MEM_R(cpu.esp, 2);
+    cpu.esp += DATA_BYTE;
+    REG(1) = MEM_R(cpu.esp, 2);
+    cpu.esp += DATA_BYTE;
+    REG(0) = MEM_R(cpu.esp, 2);
+    cpu.esp += DATA_BYTE;
+    print_asm("pusha");
 	
 	return 1;
 }
