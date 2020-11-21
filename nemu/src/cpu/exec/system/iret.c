2697afad2f3fc02027def1c21f928affe7c93a5e
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-21 07:47:52 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:47:52 up 8 days,  1:21,  1 user,  load average: 0.92, 1.26, 1.23 83a2b154b763f9909c1dab8815cae514a77e381b

--- nemu/src/cpu/exec/system/iret.c
+++ nemu/src/cpu/exec/system/iret.c
@@ -1,13 +1,14 @@
 #include "cpu/exec/helper.h"
 
 make_helper(iret) {
-	//if (cpu.PE == 0) {
-		cpu.eip = swaddr_read(cpu.esp, 4, 2);
-		cpu.esp += 4;
-		cpu.sreg[1].selector = swaddr_read(cpu.esp, 4, 2);
-		cpu.esp += 4;
-		cpu.eflags = swaddr_read(cpu.esp, 4, 2);
-		cpu.esp += 4;
-		return 0;
-	//}
+
+	cpu.eip = swaddr_read(cpu.esp, 4, 2);
+	cpu.esp += 4;
+	cpu.sreg[1].selector = swaddr_read(cpu.esp, 4, 2);
+	cpu.esp += 4;
+	cpu.eflags = swaddr_read(cpu.esp, 4, 2);
+	cpu.esp += 4;
+	print_asm("iret");
+	return 1;
+	
 }
