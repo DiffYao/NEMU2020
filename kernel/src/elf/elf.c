519720d7039556da188654b1f9a9361d385efecb
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-24 06:21:27 +0000

    >  compile kernel 3018216244 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:21:27 up 10 days, 23:54,  1 user,  load average: 1.62, 1.81, 1.55 f248fe83c4e7efa0fea181ede223fa66abf01eeb

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -55,7 +55,7 @@ uint32_t loader() {
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
 
-			 memset ((void *)(ph->p_vaddr + ph->p_filesz),0,ph->p_memsz -ph->p_filesz);
+			 memset((void *)(ph->p_vaddr + ph->p_filesz),0,ph->p_memsz -ph->p_filesz);
 
 
 #ifdef IA32_PAGE
