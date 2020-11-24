268619f4ea0b904ce3a2dbc9ebd87bd1ca8c5077
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-24 06:23:47 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:23:47 up 10 days, 23:57,  1 user,  load average: 1.66, 1.77, 1.58 b309054222ea61b8cdc3d65ab2c5d952b5ea7c5a

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -58,7 +58,7 @@ hwaddr_t page_translate(lnaddr_t addr) {
 			page_entry = (page_entry << 12) | (page << 2);
 			PTE pte;
 			pte.val = hwaddr_read(page_entry, 4);
-			assert(pte.present == 1);
+			//assert(pte.present == 1);
 			hwaddr = offset | (pte.page_frame << 12);
 			TLB_update(addr, hwaddr);
 		}
