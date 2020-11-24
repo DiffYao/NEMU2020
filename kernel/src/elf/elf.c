231ceb9606c6b15f7abc4bc3bf352244dbbf5d74
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-24 06:16:08 +0000

    >  compile kernel 3018216244 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:16:08 up 10 days, 23:49,  1 user,  load average: 1.97, 1.90, 1.46 db33d050e3b6977ef984fe80c13fad716bd142c

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -2,6 +2,7 @@
 #include "memory.h"
 #include <string.h>
 #include <elf.h>
+#include <stdio.h>
 
 #define ELF_OFFSET_IN_DISK 0
 
@@ -21,52 +22,51 @@ uint32_t loader() {
 	Elf32_Ehdr *elf;
 	Elf32_Phdr *ph = NULL;
 	uint8_t buf[4096];
+
 #ifdef HAS_DEVICE
 	ide_read(buf, ELF_OFFSET_IN_DISK, 4096);
 #else
 	ramdisk_read(buf, ELF_OFFSET_IN_DISK, 4096);
 #endif
-
+	
 	elf = (void*)buf;
-
 	/* TODO: fix the magic number with the correct one */
 	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	int loop_var = 0;
-	ph = (void*)((uint8_t*)buf + elf->e_phoff);
-	//nemu_assert((ph + 1) == (void*)((uint8_t*)buf + elf->e_phoff) + elf->e_phentsize); //this is true
-	for(; loop_var < elf->e_phnum; loop_var++) {
+	int i;
+	ph = (void *)(buf+elf->e_phoff) ;
+	for(i = 0; i < elf->e_phnum ; ++i, ++ph) {
 		/* Scan the program header table, load each segment into memory */
-		ph = ph + loop_var;
 		if(ph->p_type == PT_LOAD) {
+
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			//uint8_t buff[4096];
-			uint32_t addr = mm_malloc(ph->p_vaddr, ph->p_memsz);
+			ph->p_vaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
 #ifdef HAS_DEVICE
-			ide_read((uint8_t*)addr, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
-#else
-			ramdisk_read((void*)addr, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
+			ide_read((void *)ph->p_vaddr,ph->p_offset,ph->p_filesz);
+#else	
+			ramdisk_read ((void *)ph->p_vaddr,ph->p_offset,ph->p_filesz);
 #endif
-			//memcpy((void*)ph->p_vaddr, buff, ph->p_filesz);
-
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			memset((void*)addr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+
+			 memset ((void *)(ph->p_vaddr + ph->p_filesz),0,ph->p_memsz -ph->p_filesz);
+
+
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
 			extern uint32_t cur_brk, max_brk;
 			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
-			if(cur_brk < new_brk) { max_brk = cur_brk = new_brk; }
+			if(cur_brk < new_brk) { cur_brk = new_brk; max_brk = new_brk; }
 #endif
 		}
 	}
-	
+
 	volatile uint32_t entry = elf->e_entry;
 
 #ifdef IA32_PAGE
@@ -78,5 +78,6 @@ uint32_t loader() {
 
 	write_cr3(get_ucr3());
 #endif
+
 	return entry;
 }
