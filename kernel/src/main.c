b252e32ad3bcc602d8ac58be54c2c65071e790ce
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-28 17:28:48 +0000

    >  compile kernel 3018216244 simpleedu Linux simpleedu 4.15.0-128-generic #131-Ubuntu SMP Wed Dec 9 06:57:35 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:28:48 up  1:59,  1 user,  load average: 0.84, 0.80, 0.85 b83d0b5ca4f7f06cda012962d9722596ddab97d

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -71,9 +71,9 @@ void init_cond() {
 	 * Note that the output is actually performed only when
 	 * the serial port is available in NEMU.
 	 */
-	set_bp();
+	//set_bp();
 	Log("Hello, NEMU World ");
-	set_bp();
+	//set_bp();
 
 	Log("test\n");
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
@@ -81,10 +81,10 @@ void init_cond() {
 	video_mapping_write_test();
 #endif
 	/* Load the program. */
-	set_bp();
+	//set_bp();
 	uint32_t eip = loader();
 	Log("eip is 0x%x\n", eip);
-	set_bp();
+	//set_bp();
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Read data in the video memory to check whether 
 	 * the test data is written sucessfully.
