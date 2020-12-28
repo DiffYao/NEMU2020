9fa0a260afd9d3c0b6a817052b35c3d8e225ff24
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-28 17:35:00 +0000

    >  compile kernel 3018216244 simpleedu Linux simpleedu 4.15.0-128-generic #131-Ubuntu SMP Wed Dec 9 06:57:35 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:35:00 up  2:05,  1 user,  load average: 1.38, 1.17, 1.00 58dff43cf5a5845ce2a334fbcef0c984cecf2718

--- kernel/src/driver/ide/ide.c
+++ kernel/src/driver/ide/ide.c
@@ -17,10 +17,10 @@ void add_irq_handle(int, void (*)(void));
 void ide_read(uint8_t *buf, uint32_t offset, uint32_t len) {
 	uint32_t i;
 	for (i = 0; i < len; i ++) {
-		if (i == 0) set_bp();
+		if (i == 0) 
 		buf[i] = read_byte(offset + i);
 	}
-	set_bp();
+	
 }
 
 void ide_write(uint8_t *buf, uint32_t offset, uint32_t len) {
