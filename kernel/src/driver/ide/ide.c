411e7379210bda17cd0d69c02bcff8d7aa019620
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-28 13:13:31 +0000

    >  compile kernel 3018216244 simpleedu Linux simpleedu 4.15.0-128-generic #131-Ubuntu SMP Wed Dec 9 06:57:35 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:13:31 up  4:07,  1 user,  load average: 1.14, 1.26, 1.01 117a42b4e23029ae691c9c762f318dfc1abab

--- kernel/src/driver/ide/ide.c
+++ kernel/src/driver/ide/ide.c
@@ -18,8 +18,9 @@ void ide_read(uint8_t *buf, uint32_t offset, uint32_t len) {
 	uint32_t i;
 	for (i = 0; i < len; i ++) {
 		buf[i] = read_byte(offset + i);
-		set_bp();
+		
 	}
+	set_bp();
 }
 
 void ide_write(uint8_t *buf, uint32_t offset, uint32_t len) {
