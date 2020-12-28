55bbb68dbcedef6474465fdffd0eb3881053579b
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-28 14:33:34 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-128-generic #131-Ubuntu SMP Wed Dec 9 06:57:35 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:33:34 up  5:27,  1 user,  load average: 1.76, 1.54, 1.36 fa0738cfadaa68f0827e5bcb6cfacc4fcd105b30

--- nemu/src/cpu/exec/data-mov/mov-template.h
+++ nemu/src/cpu/exec/data-mov/mov-template.h
@@ -3,7 +3,7 @@
 
 
 static void do_execute() {
-	OPERAND_W(op_dest, op_src->val);
+	OPERAND_W(op_dest, (uint32_t)op_src->val);
 	printf("val = 0x%x, su is %d\n", op_src->val, DATA_BYTE);
 	print_asm_template2();
 }
