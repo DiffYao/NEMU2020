ee27414df2e614571f9f777bd14d14ba1464f492
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-29 11:51:28 +0000

    >  compile NEMU 3018216244 simpleedu Linux simpleedu 4.15.0-128-generic #131-Ubuntu SMP Wed Dec 9 06:57:35 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:51:28 up 21 min,  1 user,  load average: 1.89, 1.47, 1.04 652264dc56382e0df2f7cc53b227c84cc15df17d

--- nemu/src/cpu/exec/data-mov/mov-template.h
+++ nemu/src/cpu/exec/data-mov/mov-template.h
@@ -15,8 +15,9 @@ static void do_execute() {
 	printf("size = %lu ", op_dest->size);
 	printf("sreg = %d \n", op_dest->sreg);
 	OPERAND_W(op_dest, op_src->val);
-	uint32_t ad = 0xc012813e;
-	if ( ad < (1 << (10 + 10 + 3 + (27 - 10 - 10 - 3))) )printf("value = 0x%x ", swaddr_read(ad, 4, 3));
+	uint32_t ad = 0xc012183e;
+	if ( ad < (1 << (10 + 10 + 3 + (27 - 10 - 10 - 3))) )
+		printf("value = 0x%x ", swaddr_read(ad, 4, 3));
 	print_asm_template2();
 }
 
